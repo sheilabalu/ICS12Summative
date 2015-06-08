4a0b0455ec33ab2a5a8312f3eb0a8399ec762362
Legendarypotato <liueva269@gmail.com>
2015-06-08 09:12:00 -0400

    Moved it

---
+++ Life.java
@@ -0,0 +1,46 @@
+import java.awt.Graphics;
+
+
+public class Life
+{
+	private int health, attack, x, y, s, pic = 0;
+	private Type me;
+	private double armor, regenRate;
+	
+	public Life(char c, int posx, int posy)
+	{
+		me = Type.parse(c);
+		health = (int)(me.getInitHealth()*(Math.random()/5+0.9));
+		armor = (me.getInitArmor()*(Math.random()/5+0.9));
+		regenRate = (me.getInitRegenRate()*(Math.random()/5+0.9));
+		attack = (int)(me.getAttack()*(Math.random()/5+0.9));
+		
+		x = posx;
+		y = posy;
+		s = me.getSize();
+	}
+	public Life(char c)
+	{
+		me = Type.parse(c);
+		health = (int)(me.getInitHealth()*(Math.random()/5+0.9));
+		armor = (me.getInitArmor()*(Math.random()/5+0.9));
+		regenRate = (me.getInitRegenRate()*(Math.random()/5+0.9));
+		attack = (int)(me.getAttack()*(Math.random()/5+0.9));
+		
+		x = 50;
+		y = 50;
+		s = me.getSize();
+	}
+	
+	public void move(){};
+	public void drawImg(Graphics g){g.drawImage(me.getImg(pic), x, y, s-1, s-1, null);}
+	public void hurt(int attack){health-=((int)(attack*(1-armor)));}
+	public void regen(){health+=((int)(Math.random()*me.getInitHealth()*regenRate));}
+	public int attack(){return (int)(attack*(Math.random()/2+0.75));}
+	public int getHealth(){return health;}
+}
+/*
+if(health <= 0)
+	.remove(i);
+
+ */
---
+++ PlayGame.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class PlayGame extends JFrame
+{
+	public Container pane = getContentPane ();
+	public Map map;
+	public PlayGame ()
+	{
+		pane.setPreferredSize(new Dimension(1000,600));
+		DrawBoard drawboard= new DrawBoard (1000,600);
+		pane.setLayout(new BorderLayout());
+		map=new Map ("Map2");
+		//drawboard.repaint();
+		pane.add(drawboard);
+	}
+
+	public class DrawBoard extends JPanel
+	{
+		public DrawBoard (int x, int y)
+		{
+			super ();
+			setPreferredSize(new Dimension(x,y));
+		}
+
+		public void paintComponent (Graphics g)
+		{
+			map.show(g);
+		}
+	}
+	public static void main (String[] args)
+	{
+		PlayGame playGame = new PlayGame ();
+		playGame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		playGame.pack ();
+		playGame.setVisible (true);
+		playGame.play();
+	}
+	private void play(){
+		
+		
+	}
+}
