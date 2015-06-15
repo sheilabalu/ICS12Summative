908cfbcced00348086c36bc0c379e5aa1c088b5e
Legendarypotato <liueva269@gmail.com>
2015-06-14 21:08:27 -0400

    Removed Excess stuff
    and altered type

--- src/Type.java
+++ src/Type.java
@@ -7,15 +7,15 @@ import javax.imageio.ImageIO;
 
 public enum Type
 {
-	GHOST(			'g', "Ghost", 			10, 0.75, 0.2,  8,  25),
-	MOUSE(			'm', "Mouse", 			35, 0,    0.01, 20, 35),
-	MUSHROOM(		'M', "Mushroom",		25, 0.2,  0.07, 5,  35),
-	PENGUIN(		'p', "Penguin", 		45, 0.3,  0.03, 12,  30),
-	PIG(			'P', "Pig", 			64, 0.4,  0.04, 32, 25),
-	SNAIL(			's', "Snail", 			25, 0.1,  0.3,  10,  30),
-	SLIME(			'S', "Slime", 			10, 0, 	  0.01, 7,  20),
-	ZOMBIEMUSHROOM(	'z', "ZombieMushroom", 	15, 0.1,  0.25, 4,  35),
-	DRAGON(			'D', "Dragon", 			255,0.42, 0.1,  42, 40),
+	GHOST(			'g', "Ghost", 			10, 1, 0,  8,  25),
+	MOUSE(			'm', "Mouse", 			35, 1,    0, 20, 35),
+	MUSHROOM(		'M', "Mushroom",		25, 1,  0, 5,  35),
+	PENGUIN(		'p', "Penguin", 		45, 1,  0, 12,  30),
+	PIG(			'P', "Pig", 			64, 1,  0, 32, 25),
+	SNAIL(			's', "Snail", 			25, 1,  0,  10,  30),
+	SLIME(			'S', "Slime", 			10, 0, 	0, 7,  20),
+	ZOMBIEMUSHROOM(	'z', "ZombieMushroom", 	15, 1,  0, 4,  35),
+	DRAGON(			'D', "Dragon", 			255,10, 1,  42, 40),
 	
 	
 	
@@ -27,12 +27,13 @@ public enum Type
 	BLANK(			'_', "Blank", 			0,  0,    0,   0,	0 );
 	
 	private final char chIdentifier;
-	private final int health, attack, size;
-	private final double armor, regenRate;
+	private int health;
+	private final int attack, size;
+	private final int armor, regenRate;
 	private final String name;
-	private final BufferedImage[] bi = new BufferedImage[4];
+
 	
-	private Type(char c, String n, int hp, double arm, double reg, int atk, int s)
+	private Type(char c, String n, int hp, int arm, int reg, int atk, int s)
 	{
 		chIdentifier = c;
 		name = n;
@@ -42,14 +43,6 @@ public enum Type
 		attack = atk;
 		size = s;
 		
-		try {
-			bi[0] = ImageIO.read (new File ("Monsters/"+name+"_MoveL.png"));
-			bi[1] = ImageIO.read (new File ("Monsters/"+name+"_DieL.png"));
-			//bi[2] = ImageIO.read (new File ("Monsters/"+name+"_MoveR.png"));
-			//bi[3] = ImageIO.read (new File ("Monsters/"+name+"_DieR.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 	
 	
@@ -74,11 +67,39 @@ public enum Type
 	}
 
 
-	public int getInitHealth(){return health;}
-	public double getInitArmor(){return armor;}
-	public double getInitRegenRate() {return regenRate;}
-	public double getAttack(){return attack;}
-	public BufferedImage getImg(int i){return bi[i];}
-	public int getSize() {return size;}
+	public int getHealth()
+	{
+		return health;
+	}
+	
+	public int getArmor()
+	{
+		return armor;
+	}
+	
+	public int getRegenRate() 
+	{
+		return regenRate;
+	}
+	
+	public int getAttack()
+	{
+		return attack;
+	}
+	
+	public int getSize() 
+	{
+		return size;
+	}
+	
+	public int hurt (Player player)
+	{
+		if (player.getAttack()>armor)
+		{
+			health+=armor-player.getAttack();
+			return armor-player.getAttack();
+		}
+		return 0;
+	}
 
 }
